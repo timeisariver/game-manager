ba8dae3dffc9bb28b58e7826efc41cd9366a0371
nk3124 <timesariver@evsg-dsgn.com>
2026-04-19 08:29:35 +0900

    react setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+const QueryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={QueryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
