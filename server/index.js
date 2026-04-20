ed700f9c86912f15937b8f96a15f116b3a1a8ab7
nk3124 <timesariver@evsg-dsgn.com>
2026-04-21 07:21:32 +0900

    Fetch Game By Id

--- server/index.js
+++ server/index.js
@@ -10,12 +10,25 @@ app.get("/api/games", (req, res) => {
   res.json(games);
 });
 
+app.get("/api/games/:id", (req, res) => {
+  const { id } = req.params;
+  const game = games.find((g) => {
+    g.id === Number(id)
+  });
+
+  if (!game) {
+    return res.status(404).json({ error: "Game not found." });
+  }
+
+  res.json(game);
+});
+
+
 app.post("/api/games", (req, res) => {
   const newGame = req.body;
   games.push({ ...newGame, id: games.length + 1 });
   res.status(201).json(newGame);
 });
-
 
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
