c3880458918f93232399c17e8b833c2d93eda522
nk3124 <timesariver@evsg-dsgn.com>
2026-04-26 22:09:16 +0900

    404

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div
+      className="page-container"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>404</h1>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
 }
 
 export default NotFound;
